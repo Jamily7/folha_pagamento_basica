957bf81ce66c86f9b7bf7fc6c2d9c8de01f479d0
jamily <jamilysouzarosa5002@gmail.com>
2026-05-21 23:29:32 -0300

    Add project files.

---
+++ folha_pagamento_basica/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+
+int numeroFuncionario, horasTrabalhadas;
+double valorPorHora;
+
+Console.WriteLine("Digite o número funcionário (**):");
+numeroFuncionario = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número de horas trabalhadas:");
+horasTrabalhadas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor que recebe por hora:");
+valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+double salario = horasTrabalhadas *  valorPorHora;
+
+Console.WriteLine("Número Funcionário:" + numeroFuncionario);
+Console.WriteLine("Salário: R$ " + salario.ToString("F2")); 
